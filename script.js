dd349c1fe0a5701110876c0dde00e3202144dc58
FreedoMenjoy <122691936+FreedoMenjoy@users.noreply.github.com>
2023-01-17 20:02:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+/*'use strict';
+ global document 
+var checkboxElement = document.querySelector('#checkbox');
+var menuOutfitElement = document.querySelector('.menu-outfit');
+menuOutfitElement.onclick = function () {
+    checkboxElement.checked = !checkboxElement.checked;
+};*/
 'use strict';
-/* global document */
+/* global document alert*/ 
 var checkboxElement = document.querySelector('#checkbox');
 var menuOutfitElement = document.querySelector('.menu-outfit');
 menuOutfitElement.onclick = function () {
     checkboxElement.checked = !checkboxElement.checked;
 };
+if (checkboxElement == null)
+    alert('DEBUG: Checkbox button element not found');
+if (menuOutfitElement == null)
+    alert('DEBUG: Menu button element not found');
+if (menuOutfitElement != null) {
+    menuOutfitElement.addEventListener('click', function () {
+        if (checkboxElement != null)
+            checkboxElement.checked = !checkboxElement.checked;
+    });
+}
