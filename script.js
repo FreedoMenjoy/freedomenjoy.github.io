1b75e52dbda410201bdacc7d7be00529f1247623
Maksim Shamber <36640388+nowifi4u@users.noreply.github.com>
2023-02-24 22:09:45 +0300

    refactor

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
 'use strict';
-var checkboxElement = null;
-var menuOutfitElement = null;
 /* global document alert */
-try {
-    checkboxElement = document.querySelector('#checkbox');
-    menuOutfitElement = document.querySelector('.menu-outfit');
-    if (checkboxElement == null)
-        alert('DEBUG: Checkbox button element not found');
-    if (menuOutfitElement == null)
-        alert('DEBUG: Menu button element not found');
-    if (menuOutfitElement != null) {
-        menuOutfitElement.addEventListener('click', (event) => {
-            event.stopPropagation();
-            if (checkboxElement != null)
-                checkboxElement.checked = !checkboxElement.checked;
-        });
-    }
-}
-catch (err) {
-    alert(`ERROR ${String(err)}`);
-}
+const checkboxElement = document.querySelector('#checkbox');
+checkboxElement.checked; // force error state before click
+const menuOutfitElement = document.querySelector('.menu-outfit');
+menuOutfitElement.addEventListener('click', (event) => {
+    if (checkboxElement != null)
+        checkboxElement.checked = !checkboxElement.checked;
+});
