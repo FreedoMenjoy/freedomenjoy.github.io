adb5e76428243224c9092196c477c24a07dee2d2
Maxim Shamber <36640388+nowifi4u@users.noreply.github.com>
2023-02-24 21:26:54 +0300

    feat: score display

--- script4.js
+++ script4.js
@@ -1,14 +1,19 @@
-let button = document.querySelector('#button');
+const buttonElement = document.querySelector('#button');
+const scoreElement = document.querySelector('#score');
 
 
-button.addEventListener('click', function() {
+buttonElement.addEventListener('click', function() {
 	let inputs = document.querySelectorAll('#test input');
 	
+	let correct = 0;
 	for (let input of inputs) {
 		if (input.value == input.dataset.right) {
 			input.classList.add('correct');
+			correct++;
 		} else {
 			input.classList.add('incorrect');
 		}
 	}
+	
+	scoreElement.innerText = String(correct);
 });
