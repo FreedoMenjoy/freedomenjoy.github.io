d6578ff4447488137e8033c81240c68c94f46c1d
Maksim Shamber <36640388+nowifi4u@users.noreply.github.com>
2023-03-12 00:18:14 +0300

    feat: shuffleArray

--- src/scripts/util/math.ts
+++ src/scripts/util/math.ts
@@ -7,3 +7,18 @@ export function clamp (min: number, value: number, max: number): number {
   if (value < min) return min;
   return value;
 }
+
+export function shuffleArrayInplace<T> (array: T[]): void {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
+export function shuffleArray<T> (array: T[]): T[] {
+  const newarray = Array.from(array);
+  shuffleArrayInplace(newarray);
+  return newarray;
+}
