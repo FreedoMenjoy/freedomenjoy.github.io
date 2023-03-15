c4da4da6f5749c449eed556e5edd12de7f70ee8d
Maksim Shamber <36640388+nowifi4u@users.noreply.github.com>
2023-03-15 23:54:11 +0300

    feat: requestFullscreen

---
+++ src/scripts/util/util.ts
@@ -0,0 +1,27 @@
+import { forceGetElementById, forceQuerySelector } from './forceQuerySelector';
+
+interface HTMLElement {
+  requestFullscreen?: () => void | Promise<void>;
+  webkitRequestFullscreen?: () => void | Promise<void>;
+  mozRequestFullscreen?: () => void | Promise<void>;
+}
+
+export async function requestFullscreen (element: HTMLElement): Promise<void> {
+  if (element.requestFullscreen) {
+    await element.requestFullscreen();
+  } else if (element.webkitRequestFullscreen) {
+    await element.webkitRequestFullscreen();
+  } else if (element.mozRequestFullscreen) {
+    await element.mozRequestFullscreen();
+  }
+}
+
+export async function forceQuerySelectorRequestFullscreen (selector: string, origin?: Pick<globalThis.HTMLElement, 'querySelector'>): Promise<void> {
+  const element = forceQuerySelector(selector, origin);
+  await requestFullscreen(element);
+}
+
+export async function forceGetElementByIdRequestFullscreen (elementId: string, origin?: NonElementParentNode): Promise<void> {
+  const element = forceGetElementById(elementId, origin);
+  await requestFullscreen(element);
+}
