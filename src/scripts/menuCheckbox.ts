5db32a21aab6300e2c77297bab63a95bafe1e1f0
Maksim Shamber <36640388+nowifi4u@users.noreply.github.com>
2023-03-22 11:56:39 +0300

    refactor: rename menu checkbox id to "menu-checkbox"

--- src/scripts/menuCheckbox.ts
+++ src/scripts/menuCheckbox.ts
@@ -1,6 +1,6 @@
 import { forceQuerySelector } from './util/forceQuerySelector';
 
-const checkboxElement = forceQuerySelector<HTMLInputElement>('#checkbox');
+const checkboxElement = forceQuerySelector<HTMLInputElement>('#menu-checkbox');
 const menuOutfitElement = forceQuerySelector('.menu-outfit');
 menuOutfitElement.addEventListener('click', () => {
   if (checkboxElement != null) { checkboxElement.checked = !checkboxElement.checked; }
